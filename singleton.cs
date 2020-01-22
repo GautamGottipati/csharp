e30d7314a962e05031e51acc1475072cce03545b
Gottipati Gautam <38732642+GautamGottipati@users.noreply.github.com>
2020-01-22 11:26:09 +0530

    removed non singleton class

--- singleton.cs
+++ singleton.cs
@@ -3,26 +3,6 @@ public interface ISingletonContainer
     int GetPopulation(string name);
 }
 
-public class SingletonDataContainer: ISingletonContainer
-{
-    private Dictionary<string, int> _capitals = new Dictionary<string, int>();
- 
-    public SingletonDataContainer()
-    {
-        Console.WriteLine("Initializing singleton object");
- 
-        var elements = File.ReadAllLines("capitals.txt");
-        for (int i = 0; i < elements.Length; i+=2)
-        {
-            _capitals.Add(elements[i], int.Parse(elements[i + 1]));
-        }
-    }
- 
-    public int GetPopulation(string name)
-    {
-        return _capitals[name];
-    }
-}
 // Singleton class -> contains private constructor(SingletonDataContainer),static methods 
 public class SingletonDataContainer: ISingletonContainer
 {
